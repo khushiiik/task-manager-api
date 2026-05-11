786b77e73ece9266fbbd83cd2308bfe75e5e99a8
khushiiik <khushikoriya118@gmail.com>
2026-05-11 18:38:32 +0530

    Add permission class to teams module

--- teams/views.py
+++ teams/views.py
@@ -1,14 +1,17 @@
 from rest_framework.viewsets import ModelViewSet
-from teams.serializers import TeamSerializer
-from teams.models import Team
 from rest_framework.decorators import action
-from accounts.serializers import UserSerializer
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from teams.serializers import TeamSerializer
+from accounts.serializers import UserSerializer
+from teams.permissions import TeamPermission
+from teams.models import Team
 
 
 class TeamViewSet(ModelViewSet):
-    queryset = Team.objects.all()
     serializer_class = TeamSerializer
+    permission_classes = [IsAuthenticated, TeamPermission]
+    queryset = Team.objects.all()
 
     @action(detail=True, methods=["get"])
     def members(self, request, pk=None):
